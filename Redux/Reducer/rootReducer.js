e9fa80c90d48311a9fd7a5195f23fc4837623ac1
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2024-05-18 02:14:43 +0600

    Update rootReducer.js

--- Redux/Reducer/rootReducer.js
+++ Redux/Reducer/rootReducer.js
@@ -1,15 +1,9 @@
 // src/reducers/index.js
 import { combineReducers } from "redux";
 import tableDataSlice from "../Slices/TableDataSlice";
-import poNumberSlice from "../Slices/poNumberSlice";
-import tableSelectedRow from "../Slices/tableSelectedRow";
-import leadsData from "../Slices/leadsData";
 
 const rootReducer = combineReducers({
   tableData: tableDataSlice,
-  po: poNumberSlice,
-  selectedRow: tableSelectedRow,
-  leadsRow: leadsData,
 });
 
 export default rootReducer;
